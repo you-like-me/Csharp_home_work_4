ae281c427fd97f0ff88e67396a1a43b2bc4dfed7
katerina <you-like-me@yandex.ru>
2022-08-19 23:48:17 +0300

    Add Task 027

---
+++ Task_27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int i = 0;
+    while (a > 0)
+    {
+        sum = sum + a % 10; 
+        a = a / 10;
+        i++;
+    }
+    
+Console.WriteLine(sum);
