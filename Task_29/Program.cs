a337089d664358d3d4c2fdad0df3e5ea551e03af
katerina <you-like-me@yandex.ru>
2022-08-19 22:54:55 +0300

    Add Task 029

---
+++ Task_29/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите максимальный порог значений элементов массива n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] element)
+{
+    int length = element.Length;
+    int index = 0;
+    while (index < length)
+    {
+        element[index] = new Random().Next(1, n);
+        index++;
+    }
+}
+
+void PrintArray(int[] elem)
+{
+    int count = elem.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{elem[position]}" + ", ");
+        position++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+
+PrintArray(array);
+
