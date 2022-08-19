a07ab93dcb482dba25baa209c04a8fc034b58f30
katerina <you-like-me@yandex.ru>
2022-08-19 19:42:28 +0300

    Add Task 025

---
+++ Task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число А ");
+double A = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую будет возводиться число А: В =  ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double Degree = 1;
+for (int i = 0; i < B + 1; i++)
+{
+    Degree = Math.Pow(A, B);
+}
+Console.WriteLine($"Число А в степени B = {Degree}");
